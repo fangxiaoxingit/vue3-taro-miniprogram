1fb1bddb19d74934516f2c1c0218e062f449045d
fangxiaoxin <onlyfangsiyu@outlook.com>
2022-05-18 14:07:05 +0800

    同步代码

--- src/utils/key.js
+++ src/utils/key.js
@@ -5,16 +5,19 @@ switch (NODE_ENV) {
     case 'development':
         key = {
             ossKey: '345623746527346952', //自定义环境变量
+            base: 'http://www.baidu.com', //url
         };
         break;
     case 'production':
         key = {
             ossKey: '345623746527346953', //自定义环境变量
+            base: 'http://www.google.com', //url
         };
         break;
     default:
         key = {
             ossKey: '345623746527346954', //自定义环境变量
+            base: 'http://www.baidu.com', //url
         };
         break;
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,69 +1,32 @@
 import Taro from '@tarojs/taro'
 import { hideLoading, showLoading } from './tools'
-//拦截器
-const interceptor = function (chain) {
-    const requestParams = chain.requestParams
-    console.log('requestParams', requestParams)
-    const { method, data, url } = requestParams
 
-    console.log(`http ${method || 'GET'} --> ${url} data: `, data)
-    console.log('----请求前-----')
-    //请求前处理
-    showLoading()
-
-    return chain.proceed(requestParams)
-        .then(res => {
-            console.log('result', res)
-            //请求拦截返回处理
-            hideLoading()
-            return res
-        })
-}
-
-Taro.addInterceptor(interceptor)
-// Taro.request({ url })
-
-
-//post 请求
-export function post(url, data) {
-    return new Promise((resolve, reject) => {
-        Taro.request({
-            method: 'POST',
-            url: url,
+export default {
+    baseOptions(params, method = 'GET') {
+        let { url, data } = params
+        let contentType = 'application/x-www-form-urlencoded'
+        contentType = params.contentType || contentType
+        const option = {
+            isShowLoading: false,
+            url: base + url,
             data: data,
-            header: {
-                'content-type': 'application/json' // 默认值
+            method: method,
+            header: { 'content-type': contentType, 'token': token }, // 默认contentType ,预留token
+            success(res) {
+
             },
-            success: function (res) {
-                console.log('请求成功res', res)
-                resolve(res)
-            },
-            fail: function (err) {
-                hideLoading()
-                console.log('err',err)
-                reject(err)
+            error(e) {
+                logError('api', '请求接口出现问题', e)
             }
-        })
-    })
-}''
-//get 请求
-export function get(url, data) {
-    return new Promise((resolve, reject) => {
-        Taro.request({
-            method: 'GET',
-            url: url,
-            data: data,
-            header: {
-                'content-type': 'application/json' // 默认值
-            },
-            success: function (res) {
-                resolve(res)
-            },
-            fail: function (err) {
-                hideLoading()
-                console.log(err)
-                reject(err)
-            }
-        })
-    })
+        }
+        return Taro.request(option)
+    },
+    get(url, data) {
+        let option = { url, data }
+        return this.baseOptions(option)
+    },
+    post(url, data) {
+        let params = { url, data }
+        return this.baseOptions(params, 'POST')
+    }
 }
